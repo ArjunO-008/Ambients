6e3f28217b71ca43d88698f56889114ea948c922
Arjun.O <arjundlm10@gmail.com>
2026-02-25 17:09:51 +0530

    Added clock related func in bridge

--- bridge/bridge.go
+++ bridge/bridge.go
@@ -1,9 +1,13 @@
 package bridge
 
-import "context"
+import (
+	"Ambients/core/clock"
+	"context"
+)
 
 type Bridge struct {
-	ctx context.Context
+	ctx          context.Context
+	clockService *clock.ClockService
 }
 
 func NewBridge() *Bridge {
@@ -12,4 +16,14 @@ func NewBridge() *Bridge {
 
 func (b *Bridge) SetContext(ctx context.Context) {
 	b.ctx = ctx
+
+	b.clockService = clock.NewClockService(ctx)
+}
+
+func (b *Bridge) StartClock(user24Hour bool) {
+	b.clockService.Start(user24Hour)
+}
+
+func (b *Bridge) StopClock() {
+	b.clockService.Stop()
 }
